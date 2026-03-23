86d11ea18d8747ab15b74f5fabedde5c2e548362
avinash1598 <madrupam@gmail.com>
2026-03-23 17:09:59 -0500

    Model fit quality comparison

--- HumanExpDataAnalysis/ModelFitQualityTest.m
+++ HumanExpDataAnalysis/ModelFitQualityTest.m
@@ -1,13 +1,20 @@
 close all
 clear all
 
-addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/HumanExpDataAnalysis/Utils/')
-addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/LLScriptsUtils/LLScriptsTrialData/')
-addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/PlotUtils/')
-addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/Utils/')
-addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/OptimizationUtils/')
-addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/HumanExpDataAnalysis/Utils/')
-addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/SimulationScripts/CompleteModelEstimation/GenerateSimulatedData')
+% addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/HumanExpDataAnalysis/Utils/')
+% addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/LLScriptsUtils/LLScriptsTrialData/')
+% addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/PlotUtils/')
+% addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/Utils/')
+% addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/OptimizationUtils/')
+% addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/HumanExpDataAnalysis/Utils/')
+% addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/SimulationScripts/CompleteModelEstimation/GenerateSimulatedData')
+
+addpath('C:\Users\avinash1598\Desktop\Uncertainty\HumanExpDataAnalysis\Utils\')
+addpath('C:\Users\avinash1598\Desktop\Uncertainty\LLScriptsUtils\LLScriptsTrialData\')
+addpath('C:\Users\avinash1598\Desktop\Uncertainty\PlotUtils\')
+addpath('C:\Users\avinash1598\Desktop\Uncertainty\Utils\')
+addpath('C:\Users\avinash1598\Desktop\Uncertainty\OptimizationUtils\')
+addpath('C:\Users\avinash1598\Desktop\Uncertainty\SimulationScripts\CompleteModelEstimation\GenerateSimulatedData')
 
 % addpath('C:\Users\avinash1598\Desktop\Uncertainty\HumanExpDataAnalysis/Utils\')
 % addpath('C:\Users\avinash1598\Desktop\Uncertainty\LLScriptsUtils\LLScriptsTrialData\')
@@ -35,7 +42,7 @@ n_uncertainty_levels = formattedData.n_uncertainty_levels; % Hard code for now
 %% Cov model
 
 % ----------------------------------------
-optParams.nStarts = 1; %300;
+optParams.nStarts = 300; %300;
 resultReducedCov = Optimize(formattedData, initCond, "cov", [], optParams, "reduced");
 
 % Simulated data
@@ -46,11 +53,11 @@ data     = GenerateCovData( params, n_uncertainty_levels, 'reduced', ...
     formattedData.orientations', size(formattedData.theta_resp_all, 3) );
 initCond = getInitialConditions(data);
 
-optParams.nStarts = 1; %300
+optParams.nStarts = 300; %300
 resultReducedCovSim = Optimize(data, initCond, "cov", [], optParams, 'reduced');
 
 % ----------------------------------------
-optParams.nStarts = 1; %300;
+optParams.nStarts = 300; %300;
 resultFullCov = Optimize(formattedData, initCond, "cov", [], optParams, "full");
 
 % Simulated data
@@ -61,11 +68,11 @@ data     = GenerateCovData(params, n_uncertainty_levels, 'full', ...
     formattedData.orientations', size(formattedData.theta_resp_all, 3));
 initCond = getInitialConditions(data);
 
-optParams.nStarts = 1; %300
+optParams.nStarts = 300; %300
 resultFullCovSim = Optimize(data, initCond, "cov", [], optParams, 'full');
 
 % ----------------------------------------
-optParams.nStarts = 1; %300;
+optParams.nStarts = 300; %300;
 resultFullJumboCov = Optimize(formattedData, initCond, "cov", [], optParams, "full_jumbo");
 
 % Simulated data
@@ -76,12 +83,12 @@ data     = GenerateCovData(params, n_uncertainty_levels, 'full_jumbo', ...
     formattedData.orientations', size(formattedData.theta_resp_all, 3));
 initCond = getInitialConditions(data);
 
-optParams.nStarts = 1; %300
+optParams.nStarts = 300; %300
 resultFullJumboCovSim = Optimize(data, initCond, "cov", [], optParams, 'full_jumbo');
 
 %% Ind model
 % ----------------------------------------
-optParams.nStarts = 1; %300;
+optParams.nStarts = 300; %300;
 resultReducedInd = Optimize(formattedData, initCond, "ind", [], optParams, "reduced");
 
 % Simulated data
@@ -92,11 +99,11 @@ data     = GenerateIndData(params, n_uncertainty_levels, 'reduced', ...
     formattedData.orientations', size(formattedData.theta_resp_all, 3));
 initCond = getInitialConditions(data);
 
-optParams.nStarts = 1; %300
+optParams.nStarts = 300; %300
 resultReducedIndSim = Optimize(data, initCond, "ind", [], optParams, 'reduced');
 
 % ----------------------------------------
-optParams.nStarts = 1; %300;
+optParams.nStarts = 300; %300;
 resultFullInd = Optimize(formattedData, initCond, "ind", [], optParams, "full");
 
 % Simulated data
@@ -107,11 +114,11 @@ data     = GenerateIndData(params, n_uncertainty_levels, 'full', ...
     formattedData.orientations', size(formattedData.theta_resp_all, 3));
 initCond = getInitialConditions(data);
 
-optParams.nStarts = 1; %300
+optParams.nStarts = 300; %300
 resultFullIndSim = Optimize(data, initCond, "ind", [], optParams, 'full');
 
 % ----------------------------------------
-optParams.nStarts = 1; %300;
+optParams.nStarts = 300; %300;
 resultFullJumboInd = Optimize(formattedData, initCond, "ind", [], optParams, "full_jumbo");
 
 % Simulated data
@@ -122,7 +129,7 @@ data     = GenerateIndData(params, n_uncertainty_levels, 'full_jumbo', ...
     formattedData.orientations', size(formattedData.theta_resp_all, 3));
 initCond = getInitialConditions(data);
 
-optParams.nStarts = 1; %300
+optParams.nStarts = 300; %300
 resultFullJumboIndSim = Optimize(data, initCond, "ind", [], optParams, 'full_jumbo');
 
 %% Data structures to save
