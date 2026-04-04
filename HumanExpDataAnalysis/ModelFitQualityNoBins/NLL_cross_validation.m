ecfd90d99ceb6afe7cda3e9a91fad029f9ae1bd5
avinash1598 <madrupam@gmail.com>
2026-04-04 17:15:35 -0500

    yichao cross validation

--- HumanExpDataAnalysis/ModelFitQualityNoBins/NLL_cross_validation.m
+++ HumanExpDataAnalysis/ModelFitQualityNoBins/NLL_cross_validation.m
@@ -16,11 +16,11 @@ addpath('C:\Users\avinash1598\Desktop\Uncertainty\HumanExpDataAnalysis\Utils\')
 % addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/OptimizationUtils/OptimizationScriptsNoBin')
 % addpath('/Users/avinashranjan/Desktop/UT Austin/Goris lab/Uncertainty/ProcessModel/SimulationScripts/CompleteModelEstimation/GenerateSimulatedData')
 
-expData            = load('../Data/COR33.mat');    % Akash
+% expData            = load('../Data/COR33.mat');    % Akash
 % expData            = load('../Data/COR31.mat');    % Tien
 % expData            = load('./Data/COR32.mat');     % Jiaming
 % expData            = load('../Data/CORNFB02.mat');  % Jonathan
-% expData            = load('../Data/CORNFB01.mat'); % Yichao
+expData            = load('../Data/CORNFB01.mat'); % Yichao
 
 fltData       = expData.dat( expData.dat.session > 0 , :);  % TODO: change session number
 f.dat         = fltData;
@@ -36,4 +36,4 @@ nPerm = 20; % no of data permutation
 cv_result = NLLCrossValidate(formattedData, initCond, K, nPerm, optParams);
 %%
 
-save('./CV_Data/cross_validation_Akash_full.mat', 'cv_result');
+save('./CV_Data/cross_validation_Yichao_full.mat', 'cv_result');
